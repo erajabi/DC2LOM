b624831786b76f7284da5406004f22d93ebdcc5b
Enayat <erajabi@yahoo.com>
2016-09-07 15:49:13 -0300

    Add files via upload

---
+++ IEEELOM.java
@@ -0,0 +1,121 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Rajabi
+ * Date: 9/19/15
+ * Time: 3:56 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class IEEELOM {
+    String General_Identifier;
+    String General_Title;
+    String General_Language;
+    String General_Description;
+    String General_Keyword;
+    String LifeCycle_Contribution;
+    String Technical_Format;
+    String Technical_Location;
+    String Educational_ResourceType;
+    String Right_Otherreestriction;
+    String Classification_Purpose;
+    String Classification_Taxonomy;
+    String Educational_Description;
+    String Right_Description;
+    String Relation_Type;
+    String Relation_Resource;
+    void Set_Classification_Taxonomy(String GI){
+        Classification_Taxonomy=GI;
+    }
+    String Get_Classification_Taxonomy(){
+        return Classification_Taxonomy;
+    }
+    void Set_Classification_Purpose(String GI){
+        Classification_Purpose=GI;
+    }
+    String Get_Classification_Purpose(){
+        return Classification_Purpose;
+    }
+    void Set_Educational_Description(String GI){
+        Educational_Description=GI;
+    }
+    String Get_Educational_Description(){
+        return Educational_Description;
+    }
+    void Set_Right_Description(String GI){
+        Right_Description=GI;
+    }
+    String Get_Right_Description(){
+        return Right_Description;
+    }
+    void Set_Relation_Resource(String GI){
+        Relation_Resource=GI;
+    }
+    String Get_Relation_Resource(){
+        return Relation_Resource;
+    }
+    void Set_Relation_Type(String GI){
+        Relation_Type=GI;
+    }
+    String Get_Relation_Type(){
+        return Relation_Type;
+    }
+    void Set_Right_Otherreestriction(String GI){
+        Right_Otherreestriction=GI;
+    }
+    String Get_Right_Otherreestriction(){
+        return Right_Otherreestriction;
+    }
+    void Set_Educational_ResourceType(String GI){
+        Educational_ResourceType=GI;
+    }
+    String Get_Educational_ResourceType(){
+        return Educational_ResourceType;
+    }
+    void Set_General_Identifier(String GI){
+        General_Identifier=GI;
+    }
+    String Get_General_Identifier(){
+        return General_Identifier;
+    }
+    void Set_General_Title(String GI){
+        General_Title=GI;
+    }
+    String Get_General_Title(){
+        return General_Title;
+    }
+    void Set_General_Language(String GI){
+        General_Language=GI;
+    }
+    String Get_General_Language(){
+        return General_Language;
+    }
+    void Set_General_Description(String GI){
+        General_Description=GI;
+    }
+    String Get_General_Description(){
+        return General_Description;
+    }
+    void Set_General_Keyword(String GI){
+        General_Keyword=GI;
+    }
+    String Get_General_Keyword(){
+        return General_Keyword;
+    }
+    void Set_LifeCycle_Contribution(String GI){
+        LifeCycle_Contribution=GI;
+    }
+    String Get_LifeCycle_Contribution(){
+        return LifeCycle_Contribution;
+    }
+    void Set_Technical_Format(String GI){
+        Technical_Format=GI;
+    }
+    String Get_Technical_Format(){
+        return Technical_Format;
+    }
+    void Set_Technical_Location(String GI){
+        Technical_Location=GI;
+    }
+    String Get_Technical_Location(){
+        return Technical_Location;
+    }
+}
